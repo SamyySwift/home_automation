dd0ea05c70aa589c529614e5b99fbdbb4cc2f49e
SamyySwift <Samueludofia94@gmail.com>
2022-10-01 17:28:34 +0100

    updated app

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import uvicorn
+
 
 app = FastAPI()
 
@@ -9,3 +11,7 @@ def home():
 @app.get("/api/getstate")
 def state():
     return {"state":"light is off"}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8080)
