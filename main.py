da8c6effb8c5c267d3b3d27e498f34042b059f0e
SamyySwift <Samueludofia94@gmail.com>
2022-10-01 21:01:30 +0100

    added post method

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import uvicorn
 
 
 app = FastAPI()
-
+names = []
 @app.get("/")
 def home():
     return {"Text":"Hey samuel"}
@@ -11,6 +11,12 @@ def home():
 @app.get("/api/getstate")
 def state():
     return {"state":"light is off"}
+
+@app.post("/api/username/{user_name}")
+def user(user_name: str):
+    names.append(user_name)
+    
+    return names
 
 
 if __name__ == '__main__':
