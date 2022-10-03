f5513dca4dbf836796ccae07d6607d812f7d2c98
SamyySwift <Samueludofia94@gmail.com>
2022-10-03 18:43:28 +0530

    updated files

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, File, UploadFile, BackgroundTasks, Response, Form
 import uvicorn
-
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import StreamingResponse
+import shutil
 
 app = FastAPI()
-names = []
-@app.get("/")
-def home():
-    return {"Text":"Hey samuel"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+# Render html files
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/api/getstate")
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/getstate")
 def state():
     return {"state":"light is off"}
 
-@app.post("/api/username/{user_name}")
-def user(user_name: str):
-    names.append(user_name)
-    
-    return names
+@app.post("/submitaudio")
+async def user(audio_data: UploadFile = File(...)):
+    with open('audio.wav', 'wb') as f:
+        shutil.copyfileobj(audio_data.file, f)
+    return audio_data.filename
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='127.0.0.0', port=8000)
